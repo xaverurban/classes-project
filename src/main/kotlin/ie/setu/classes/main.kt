ec8a268e5e09c676b757cd43763d427e67be132c
Xaver <urbanxaver@hotmail.com>
2023-01-31 10:04:39 +0000

    Added new function personInfo Added more persons to arraylist Added way to print out the element and the index of them

--- src/main/kotlin/ie/setu/classes/main.kt
+++ src/main/kotlin/ie/setu/classes/main.kt
@@ -1,9 +1,23 @@
 package ie.setu.classes
 
+private val persons = ArrayList<Person>()
 fun main (args: Array<String>){
+    persons.add(Person("Mark","Roche"))
+    persons.add(Person("John","Walsh"))
+    persons.add(Person("Sheila","Flynn"))
+   // persons.add(Person("",""))
+
+   // for (person in persons)
+  //  println(person)
+for ((index, value) in persons.withIndex())
+    println("$index: $value")
+}
+
+fun personInfo(){
     val person1 = Person("Joe","Roach")
     val person2 = person1.copy(firstName = "Mark")
     val person3 = person1.copy()
+
     println(person1)
     println(person2.toString())
 
