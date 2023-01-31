48078567f4c0ce9b955accd22b52c243921e30e4
Xaver <urbanxaver@hotmail.com>
2023-01-31 09:49:54 +0000

    Added function to copy person into another and replace eg. first name

--- src/main/kotlin/ie/setu/classes/Person.kt
+++ src/main/kotlin/ie/setu/classes/Person.kt
@@ -1,4 +1,5 @@
 package ie.setu.classes
 
-class Person {
+data class Person(var firstName: String , var lastName: String) {
+
 }
--- src/main/kotlin/ie/setu/classes/main.kt
+++ src/main/kotlin/ie/setu/classes/main.kt
@@ -1,4 +1,24 @@
 package ie.setu.classes
 
-class main {
+fun main (args: Array<String>){
+    val person1 = Person("Joe","Roach")
+    val person2 = person1.copy(firstName = "Mark")
+    val person3 = person1.copy()
+    println(person1)
+    println(person2.toString())
+
+    println("person1 hashcode= ${person1.hashCode()}")
+    println("person2 hashcode = ${person2.hashCode()}")
+    println("person3 hashcode= ${person3.hashCode()}")
+
+    if (person1 == person2)
+        println("person1 is equal to person2.")
+    else
+        println("person1 is not equal to person2.")
+
+    if (person1 == person3)
+        println("person1 is equal to person3.")
+    else
+        println("person1 is not equal to person3.")
+
 }
